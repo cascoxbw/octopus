a557ac4e94f5c7d920971f720954494caa7a5133
Xu, Bowen <bowen.xu@intel.com>
2024-02-24 07:44:25 +0000

    format

--- util.py
+++ util.py
@@ -18,24 +18,4 @@ class util:
         return time.strftime('%Y%m%d%H%M%S', time.localtime())
     
     def fence(*args):
-        left = '###############'
-        right = '################################################'
-        info = left
-        total = 30
-        cost = 0
-        for i in args:
-            info += ' '
-            cost += len(' ')
-            if type(i) != str:
-                info += str(i)
-                cost += len(str(i))
-            else:
-                info += i
-                cost += len(i)
-        rest = total - cost
-        for i in range(0,rest):
-            info += ' '
-        info += right
-        print(info)
-
-
+        print((' '.join([str(i) for i in args])).center(90,'#'))
